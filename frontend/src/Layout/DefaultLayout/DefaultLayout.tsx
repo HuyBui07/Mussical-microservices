b1b1500d7d8b60a594bec73e94b2af8a7a6c93d4
Shoru <bapcui596@gmail.com>
2024-03-24 17:17:30 +0700

    feat: default layout tab intergration

--- frontend/src/Layout/DefaultLayout/DefaultLayout.tsx
+++ frontend/src/Layout/DefaultLayout/DefaultLayout.tsx
@@ -1,20 +1,22 @@
-import PropTypes from 'prop-types';
-import { ReactNode } from 'react';
-import SideBar from '../../Components/SideBar';
-
+import PropTypes from "prop-types";
+import { ReactNode } from "react";
+import SideBar from "../../Components/SideBar";
+import { useState } from "react";
 function DefaultLayout({ children }: { children: ReactNode }) {
-    return (  
-        <div className="flex flex-col h-screen">
-            <div className="flex flex-grow">
-                <SideBar />
-                <div className="flex-grow ">{children}</div>
-            </div>
-        </div>
-    );
+  const [currentTab, setCurrentTab] = useState(children);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-grow">
+        <SideBar setTab={setCurrentTab} />
+        <div className="flex-grow ">{currentTab}</div>
+      </div>
+    </div>
+  );
 }
 
 DefaultLayout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default DefaultLayout;
