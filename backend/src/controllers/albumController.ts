ce155795371929d2d66016129b8bfcc597e7346c
Gia Huy <21520909@gm.uit.edu.vn>
2024-03-30 18:15:39 +0700

    ft: song and album searching

--- backend/src/controllers/albumController.ts
+++ backend/src/controllers/albumController.ts
@@ -4,8 +4,12 @@ import { AuthRequest } from "../middlewares/requireAuth";
 import Album from "../models/albumModel";
 
 const getAllAlbums = async (req: AuthRequest, res: Response): Promise<void> => {
+  const title = req.query.title as string;
+
   try {
-    const albums = await Album.find();
+    const albums = await Album.find({
+      title: { $regex: new RegExp(title), $options: "i" },
+    });
     res.status(200).json(albums);
   } catch (err: any) {
     res.status(400).json({ message: err.message });
