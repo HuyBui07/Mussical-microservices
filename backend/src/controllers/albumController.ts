f3af088b9aa9902a31e21a8e0af002147827e657
Gia Huy <21520909@gm.uit.edu.vn>
2024-04-06 19:08:59 +0700

    ft: add manager role and actions

--- backend/src/controllers/albumController.ts
+++ backend/src/controllers/albumController.ts
@@ -16,4 +16,39 @@ const getAllAlbums = async (req: AuthRequest, res: Response): Promise<void> => {
   }
 };
 
-export { getAllAlbums };
+const createAlbum = async (req: AuthRequest, res: Response): Promise<void> => {
+  const { albumTitle, albumPoster } = req.body;
+
+  try {
+    const album = new Album({ albumTitle, albumPoster });
+    await album.save();
+    res.status(201).json(album);
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const addSongToAlbum = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  const { album_id, song_ids } = req.body;
+
+  try {
+    const album = await Album.findById(album_id);
+    if (!album) {
+      throw new Error("Album not found");
+    }
+
+    for (const song_id of song_ids) {
+      album.songs.push(song_id);
+    }
+
+    await album.save();
+    res.status(200).json(album);
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export { getAllAlbums, createAlbum, addSongToAlbum };
--- backend/src/models/albumModel.ts
+++ backend/src/models/albumModel.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const playlistSchema = new mongoose.Schema({
   name: { type: String, required: true },
   songs: { type: [String], required: true },
-  cover_url: { type: String, required: true },
+  poster: { type: String, required: true },
 });
 
 export default mongoose.model("Album", playlistSchema);
