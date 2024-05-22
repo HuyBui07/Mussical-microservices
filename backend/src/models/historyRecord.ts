975c9f9b13a709b248f49df6038af2012a30137d
Shoru <bapcui596@gmail.com>
2024-05-22 22:54:41 +0700

    chore : added adding history record to user post creation of said record

--- backend/src/models/historyRecord.ts
+++ backend/src/models/historyRecord.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export interface HistoryRecord {
+  _id: string;
   userId: string;
   songId: string;
   dateListened: Date;
@@ -10,6 +11,13 @@ const HistoryRecordSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   songId: { type: mongoose.Schema.Types.ObjectId, ref: "Song", required: true },
   dateListened: { type: Date, required: false, default: Date.now },
+});
+
+//After: add a history record id to user model after being created
+HistoryRecordSchema.post("save", async function (doc: HistoryRecord) {
+  const User = mongoose.model("User");
+  console.log("Adding history record to user", doc.userId);
+  await User.updateOne({ _id: doc.userId }, { $push: { history: doc._id } });
 });
 
 export default mongoose.model<HistoryRecord>(
